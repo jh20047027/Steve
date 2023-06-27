fe3ea50135d1622e5e022ab3a95ef500fdddf73d
jh20047027 <77910143+jh20047027@users.noreply.github.com>
2023-06-27 12:30:20 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class FibonacciCalculator
+{
+    static void Main()
+    {
+        Console.Write("請輸入費式數列項數: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(Fibonacci(i));
+        }
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+
+        int a = 0;
+        int b = 1;
+        int result = 0;
+
+        for (int i = 2; i <= n; i++)
+        {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+
+        return result;
+    }
+}
